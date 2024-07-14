ab416950bf4931acb23eebc5e456670e714cf2e1
Abdulaziz <156635185+abdulazizraxmonov@users.noreply.github.com>
2024-07-14 22:59:23 +1300

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
